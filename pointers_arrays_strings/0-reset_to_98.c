ca751b7f60212c0de2dfe04192551b02be2d4b95
root <christopheroboa@gmail.com>
2023-10-23 03:44:10 -0700

    akuma

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
+#include <main.h>
 
-void reset_to_98(int *n) {
-    if (n != NULL) { // Check if the pointer is not NULL to avoid undefined behavior.
-        *n = 98; // Update the value the pointer points to 98.
-			        }
-    }
 
-int main() {
-    int number = 42;
-    int *ptr = &number;
-
-    printf("Before: %d\n", *ptr);
-		        
-    reset_to_98(ptr);
-
-    printf("After: %d\n", *ptr);
-
-    return 0;
+/**
+ * reset_to_98 - a function that takes a pointer to an int a parameter
+ * and updates the value it points to to 98
+ * @n: input
+ * return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
+
+
