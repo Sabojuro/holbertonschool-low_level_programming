6177b76a6b3a21c958a4928e2d819cae4a9c7fdd
root <christopheroboa@gmail.com>
2023-10-25 15:13:42 -0700

    luffy

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
-#include (main.h)
-
+#include "main.h"
 
 /**
- * reset_to_98 - a function that takes a pointer to an int a parameter
- * and updates the value it points to to 98
- * @n: input
- * return: n
+ * reset_to_98 - a function that takes a pointer to an int a parameter and updates the value
+ * @n: the variable
+ * return: nothing
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
