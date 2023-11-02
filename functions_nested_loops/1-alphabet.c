516bafd5c52c73286b48b31eaf24b781ef23cf93
root <christopheroboa@gmail.com>
2023-11-01 18:36:34 -0700

    pika

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  * prints_alphabet -  prints the alphabet in lowercase, followed by a new line.
+ *   */
+
+void prints_alphabet(void)
+{
+		char alphabet;
+
+			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+						_putchar(alphabet);
+
+				_putchar('\n');
+}
